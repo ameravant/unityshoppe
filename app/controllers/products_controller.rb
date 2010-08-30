2ae376ab4ed1a5c627b6605f2554275144097094
Macpiece <macpiece@Macpieces-MacBook.local>
2010-08-30 11:57:05 -0700

    changed check for google sandbox to cms bool instead

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -51,8 +51,7 @@ class ProductsController < ApplicationController
   def google_post
     if !params["product-price"].blank?
     google_params = { "_type"=>"checkout-shopping-cart", "shopping-cart.items.item-1.item-name" => params["product-attr-option"], "shopping-cart.items.item-1.item-description" => params["product-title"],"shopping-cart.items.item-1.unit-price" => params["product-price"], "shopping-cart.items.item-1.unit-price.currency" => "USD","shopping-cart.items.item-1.quantity" => "1","shopping-cart.items.item-1.merchant-item-id" =>"UNITYDONATION"}
-    @cms_config = YAML::load_file("#{RAILS_ROOT}/config/cms.yml")
-    base_uri = RAILS_ENV == "production" ? 'https://google.com' : 'https://sandbox.google.com'
+    base_uri = @cms_config["site_settings"]["google_sandbox"] ? 'https://sandbox.google.com' : 'https://google.com'
     headers = {'Content-Type' => 'application/xml;charset=UTF-8', 'Accept' => 'application/xml;charset=UTF-8'}
     options = { :body => google_params, :headers => headers , :basic_auth => {:username => @cms_config["site_settings"]["google_merchant_id"],:password => @cms_config["site_settings"]["google_merchant_key"]}}
     response = HTTParty.post(base_uri+"/checkout/api/checkout/v2/requestForm/Merchant/#{@cms_config["site_settings"]["google_merchant_id"]}", options)
@@ -71,7 +70,7 @@ class ProductsController < ApplicationController
   def google_response
     if params["serial-number"]
       options = {:body => {"_type" => "notification-history-request", "serial-number" => params["serial-number"]}, :headers => {'Content-Type' => 'application/xml;charset=UTF-8', 'Accept' => 'application/xml;charset=UTF-8'},:basic_auth => {:username =>  @cms_config["site_settings"]["google_merchant_id"],:password => @cms_config["site_settings"]["google_merchant_key"]}}
-      uri = RAILS_ENV == "production" ? 'https://checkout.google.com/api/checkout/v2/reportsForm/Merchant/' : 'https://sandbox.google.com/checkout/api/checkout/v2/reportsForm/Merchant/'
+      uri = @cms_config["site_settings"]["google_sandbox"] ? 'https://sandbox.google.com/checkout/api/checkout/v2/reportsForm/Merchant/' : 'https://checkout.google.com/api/checkout/v2/reportsForm/Merchant/'
       response = HTTParty.post(base_uri+"#{@cms_config["site_settings"]["google_merchant_id"]}", options)
       response_hash = CGI.parse(response.parsed_response)
       #If the request is of _type new-order-notification then it has the info we want
